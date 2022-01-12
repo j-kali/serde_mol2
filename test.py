a0d831e877c6709026b63c93ac1ae9d731098fd2
Jaroslaw Kalinowski <jaroslaw.kalinowski@csc.fi>
2022-01-12 12:39:57 +0200

    [#4] Refactor test.py
    - Make it more consistent with how the binary works

--- test.py
+++ test.py
@@ -5,27 +5,16 @@ import argparse
 def main(args):
     '''main...'''
 
-    if args.input:
-        if not args.read_db:
-            if args.just_read:
-                for input_file in args.input:
-                    m = serde_mol2.read_file(input_file)
-            else:
-                if not args.write_mol2:
-                    serde_mol2.read_file_to_db_batch(args.input, args.output, shm = not args.no_shm)
-                else:
-                    m = []
-                    for input_file in args.input:
-                        m += serde_mol2.read_file(input_file)
-                    serde_mol2.write_mol2(m, args.output)
-        else:
-            # we are reading db and outputting mol2
-            m = []
-            for input_file in args.input:
-                m += serde_mol2.read_db_all(input_file)
-            serde_mol2.write_mol2(m, args.output)
-    else:
-        print("No input file given...")
+    if args.input and args.sqlite:
+        serde_mol2.read_file_to_db_batch(args.input, args.sqlite, shm = not args.no_shm, desc = args.desc, comment = args.comment, compression = int(args.compress))
+
+    if args.output and args.sqlite:
+        m = serde_mol2.read_db_all(args.sqlite, desc = args.desc, comment = args.comment)
+        serde_mol2.write_mol2(m, args.output)
+
+    if args.list_desc and args.sqlite:
+        for desc in serde_mol2.desc_list(args.sqlite):
+            print(desc)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(usage=__doc__)
@@ -38,8 +27,12 @@ if __name__ == "__main__":
     parser.add_argument(
         '-o',
         '--output',
-        default='db.sqlite',
-        help="Sqlite database to write to"
+        help="mol2 files to write to"
+    )
+    parser.add_argument(
+        '-s',
+        '--sqlite',
+        help="Sqlite db to work with"
     )
     parser.add_argument(
         '-c',
@@ -48,24 +41,24 @@ if __name__ == "__main__":
         help="Sqlite database to write to"
     )
     parser.add_argument(
-        '--just-read',
-        action="store_true",
-        help="Just read the files"
-    )
-    parser.add_argument(
         '--no-shm',
         action="store_true",
         help="Do not use shm device for temporary storage"
     )
     parser.add_argument(
-        '--write-mol2',
+        '--list-desc',
         action="store_true",
         help="Do not use shm device for temporary storage"
     )
     parser.add_argument(
-        '--read-db',
-        action="store_true",
-        help="Do not use shm device for temporary storage"
+        '--desc',
+        default='',
+        help="Description to add or filter by"
+    )
+    parser.add_argument(
+        '--comment',
+        default='',
+        help="Comment to add or filter by"
     )
 
     main(parser.parse_args())
